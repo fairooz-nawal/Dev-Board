6878fa5efc672ae104cc79158ca4eda0d709b026
Syeda Fairooz Nawal <syedafairooznawal@gmail.com>
2025-03-02 01:18:35 +0600

    Adding the activity notice in the Activity log

--- taskComplete.js
+++ taskComplete.js
@@ -1,18 +1,26 @@
-// total taskCompleted number change
-
 
 // handling card complete Buttons
 const completeButton = document.querySelectorAll('.completeButton');
+// getting the notice section
+const activitySection = document.getElementById('activity');
 for (let i = 0; i < completeButton.length; i++) {
     completeButton[i].addEventListener('click', function () {
         alert("Board Updated Successfully");
         completeButton[i].setAttribute('disabled', true);
+
+        // changing the total task number
         let taskAssignedStr = document.getElementById('taskAssigned').innerText;
         let taskAssignedInt = stringToInt(taskAssignedStr);
         let totalTaskCompleted = minimizer(taskAssignedInt);
         document.getElementById('taskAssigned').innerText = "0"+totalTaskCompleted;
+
+        // adding notice of task complete in activity log
+        const header = document.getElementsByClassName('cardHeading')[i].innerText;
+        console.log(header);
+        noticeAdder(activitySection,header);
     })
 }
+
 
 
 
--- utilities.js
+++ utilities.js
@@ -1,13 +1,31 @@
-function minimizer(task){
-    if (task == 1){
-        alert ("All Task Completed");
+function minimizer(task) {
+    if (task == 1) {
+        alert("All Task Completed");
         return 0;
     }
-    else{
+    else {
         return task - 1;
-    }     
+    }
 }
 
-function stringToInt(str){
+function stringToInt(str) {
     return parseInt(str);
+}
+
+function noticeAdder(addActivity, header) {
+    
+    let date = new Date();
+
+    let n = date.toLocaleString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+
+    const noticediv = document.createElement('div');
+    const notice = document.createElement('p');
+    notice.innerText = `You have Completed the Task ${header} issued at ${n}`;
+    noticediv.appendChild(notice);
+    noticediv.classList.add('bg-blue-50','p-5','rounded-xl','mb-3');
+    addActivity.appendChild(noticediv);
+
 }
